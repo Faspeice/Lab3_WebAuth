b9587da1ea0ae50241ca92566066c579af9cccc3
faspeice <dima_kolodin@bk.ru>
2024-05-12 17:29:03 +0600

    Add unittest for login

--- Tests/CrudTests.py
+++ Tests/CrudTests.py
@@ -2,7 +2,7 @@ import os
 import unittest
 from app.crud import  add_user,check_login
 from app.init import app, db
-
+from app.models import User
 
 class TestCrud(unittest.TestCase):
     def setUp(self):
@@ -12,8 +12,9 @@ class TestCrud(unittest.TestCase):
         self.app_context = app.app_context()
         self.app_context.push()
         db.create_all()
-    def test_add_user(self):
+    def test_signup(self):
         self.assertIsNone(add_user('TestUser','TestUser@mail.ru','123456'))
+
 
     def test_add_exist_suser(self):
         self.assertIsNone(add_user('TestUser', 'TestUser@mail.ru', '123456'))
@@ -21,6 +22,18 @@ class TestCrud(unittest.TestCase):
         self.assertIsNotNone(result, "Получил None")
         self.assertDictEqual(result,{'error': 'Такой пользователь уже существует'})
 
+
+    def test_wrong_login(self):
+        self.assertIsNone(add_user('TestUser', 'TestUser@mail.ru', '123456'))
+        self.assertIsNone(check_login('TestUser','1234567'))
+        self.assertIsNone(check_login('TestUserok', '123456'))
+        self.assertIsNone(check_login('TestUser@mail.ru', '1234567'))
+        self.assertIsNone(check_login('TestUser@mail.com', '123456'))
+        self.assertIsNone(check_login('', ''))
+    def test_correct_login(self):
+        self.assertIsNone(add_user('TestUser', 'TestUser@mail.ru', '123456'))
+        self.assertIsNotNone(check_login('TestUser','123456'))
+        self.assertIsInstance(check_login('TestUser', '123456'),User)
     def tearDown(self):
         db.session.remove()
         db.drop_all()
